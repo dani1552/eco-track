757a013b7b7393ecd8e3c683f5beeea681e80920
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-10-11 16:41:44 +0900

    fix : 레이아웃 스크롤 문제 해결

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
-import { Container, Content } from "./Layout.style.js";
+import { Container, Content, GlobalStyle } from "./Layout.style.js";
 import Footer from "./Footer/index.jsx";
 
 const Layout = () => {
   return (
     <Container>
+      <GlobalStyle />
       <Content>
         <Outlet />
       </Content>
