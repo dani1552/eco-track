5e2ac7feac49a253c2bf74b1e6dfd4e2d1b9ac86
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-10-12 16:04:34 +0900

    design : 로딩 스피너 추가

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -1,8 +1,36 @@
+import styled, { keyframes } from "styled-components";
+
 function LoadingPage() {
   return (
-    <>
-      <p>loading!</p>
-    </>
+    <Container>
+      <SpinLoadingIcon />
+    </Container>
   );
 }
 export default LoadingPage;
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+export const SpinLoadingIcon = styled.div`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border: 6px solid var(--color-blue);
+  border-radius: 50%;
+
+  border-top-color: transparent;
+  animation: ${spin} 1s ease-in-out infinite;
+  margin: 15px auto;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  z-index: 2;
+  position: absolute;
+  top: 30%;
+`;
