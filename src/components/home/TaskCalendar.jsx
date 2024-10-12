434c9ada3d27a3b4cbe319f697e877d330f16195
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-10-12 16:22:10 +0900

    design : 캘린더 중앙정렬

--- src/components/home/TaskCalendar.jsx
+++ src/components/home/TaskCalendar.jsx
@@ -1,30 +1,30 @@
 import { useState } from "react";
-import { styled, createGlobalStyle } from "styled-components";
+import { styled } from "styled-components";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import moment from "moment";
 
-const GlobalStyle = createGlobalStyle`
+const CalendarWrapper = styled.div`
   /* 캘린더 기본 스타일 */
   .react-calendar {
     border-radius: 10px !important;
-    border: none !important; 
+    border: none !important;
     overflow: hidden !important;
   }
 
   /* 선택된 날짜에 대한 기본 스타일 */
   .react-calendar__tile--active {
-    background: none !important; 
-    box-shadow: none !important; 
+    background: none !important;
+    box-shadow: none !important;
   }
 
   /* 선택된 날짜 커스터마이징 */
   .react-calendar__tile--active abbr {
-    background: #f87171 !important; 
+    background: #f87171 !important;
     color: white !important;
-    padding: 15% !important; 
-    font-size: 12px !important; 
-    border-radius: 50% !important; 
+    padding: 15% !important;
+    font-size: 12px !important;
+    border-radius: 50% !important;
   }
 
   /* 오늘 날짜에 대한 커스터마이징 */
@@ -34,29 +34,20 @@ const GlobalStyle = createGlobalStyle`
     background: var(--color-lightgray) !important;
     border-radius: 10%;
   }
-
-  
-`;
-
-const CalendarWrapper = styled.div`
-  width: 100%;
 `;
 
 function TaskCalender() {
   const [value, onChange] = useState(new Date());
 
   return (
-    <>
-      <GlobalStyle />
-      <CalendarWrapper>
-        <Calendar
-          onChange={onChange}
-          value={value}
-          formatDay={(locate, date) => moment(date).format("DD")} // 날짜 뒤 "일" 텍스트 제거
-        />
-        <div>{moment(value).format("YYYY년 MM월 DD일")}</div>
-      </CalendarWrapper>
-    </>
+    <CalendarWrapper>
+      <Calendar
+        onChange={onChange}
+        value={value}
+        formatDay={(locate, date) => moment(date).format("DD")} // 날짜 뒤 "일" 텍스트 제거
+      />
+      <div>{moment(value).format("YYYY년 MM월 DD일")}</div>
+    </CalendarWrapper>
   );
 }
 
