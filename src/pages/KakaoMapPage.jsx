23db9a90f3e0d2f4a0700fcb6d093a5e316df5c9
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-10-18 04:25:20 +0900

    fix : 카카오맵 안뜨는 에러 해결
    * src 뒤에 &autoload=false 붙이기

--- src/pages/KakaoMapPage.jsx
+++ src/pages/KakaoMapPage.jsx
@@ -1,45 +1,55 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 function KakaoMapPage() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
-    const kakaoScript = document.createElement("script");
-    kakaoScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${
-      import.meta.env.VITE_KAKAO_JS_APP_KEY
-    }&libraries=services,clusterer`;
-    kakaoScript.onload = () => {
-      console.log("Kakao map script loaded successfully!");
-    };
-    document.head.appendChild(kakaoScript);
+    if (!window.kakao) {
+      const kakaoScript = document.createElement("script");
+      kakaoScript.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${
+        import.meta.env.VITE_KAKAO_JS_APP_KEY
+      }&libraries=services,clusterer`;
+      kakaoScript.onload = () => {
+        setIsLoaded(true);
+        console.log("Kakao map script loaded successfully!");
+      };
+      document.head.appendChild(kakaoScript);
+    } else {
+      setIsLoaded(true);
+      console.log("Kakao Maps API already loaded");
+    }
   }, []);
 
   return (
     <div>
-      <Map
-        center={{ lat: 37.506320759000715, lng: 127.05368251210247 }}
-        style={{
-          width: "600px",
-          height: "500px",
-          borderRadius: "20px",
-        }}
-      >
-        <MapMarker
-          position={{ lat: 37.506320759000715, lng: 127.05368251210247 }}
+      {isLoaded && (
+        <Map
+          center={{ lat: 37.506320759000715, lng: 127.05368251210247 }}
+          style={{
+            width: "600px",
+            height: "500px",
+            borderRadius: "20px",
+          }}
         >
-          <div
-            style={{
-              color: "#9971ff",
-              fontSize: "19px",
-              fontWeight: "700",
-              border: "4px solid #9971ff",
-              borderRadius: "10px",
-              padding: "2.5px",
-            }}
+          <MapMarker
+            position={{ lat: 37.506320759000715, lng: 127.05368251210247 }}
           >
-            🎬 small box 🎬
-          </div>
-        </MapMarker>
-      </Map>
+            <div
+              style={{
+                color: "#9971ff",
+                fontSize: "19px",
+                fontWeight: "700",
+                border: "4px solid #9971ff",
+                borderRadius: "10px",
+                padding: "2.5px",
+              }}
+            >
+              🎬 small box 🎬
+            </div>
+          </MapMarker>
+        </Map>
+      )}
     </div>
   );
 }
