089f80f325ddff4f2a68a37e4253e2e4b3607da1
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-10-21 11:15:20 +0900

    style : 홈 화면 모달 팝업창 UI

--- src/components/home/CompletedPopup.jsx
+++ src/components/home/CompletedPopup.jsx
@@ -1,57 +1,7 @@
 import { useEffect, useState } from "react";
 import Modal from "react-modal";
 import styled from "styled-components";
-
-const StyledModal = styled(Modal)`
-  width: 300px;
-  height: 200px;
-  background-color: white;
-  margin: auto;
-  padding: 20px;
-  border-radius: 10px;
-  text-align: center;
-  position: relative;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.75); /* 반투명 검정색 */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const TitleText = styled.p`
-  width: 100%;
-  font-size: 18px;
-  font-weight: var(--weight-bold);
-`;
-
-const SubTitleText = styled.p`
-  font-size: 16px;
-  font-weight: var(--weight-semi-bold);
-`;
-
-const Button = styled.button`
-  padding: 10px 20px;
-  border: none;
-  border: 1px solid #007bff;
-  color: #007bff;
-  border-radius: 5px;
-
-  cursor: pointer;
-  font-weight: var(--weight-semi-bold);
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`;
+import BirthdayIcon from "/src/assets/icons/birthday-icon.png";
 
 function CompletedPopup({ isCompleted }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -64,12 +14,12 @@ function CompletedPopup({ isCompleted }) {
 
   const openModal = () => {
     setIsOpen(true);
-    document.getElementById("root").insert = true; // 백그라운드 비활성화
+    document.getElementById("root").inert = true; // 백그라운드 비활성화
   };
 
   const closeModal = () => {
     setIsOpen(false);
-    document.getElementById("root").insert = false; // 백그라운드 비활성화 해제
+    document.getElementById("root").inert = false; // 백그라운드 비활성화 해제
   };
 
   return (
@@ -83,8 +33,12 @@ function CompletedPopup({ isCompleted }) {
           )}
         >
           <TitleText>축하해요!</TitleText>
-          <SubTitleText>작은 목표들이 모여 큰 성과를 이뤘어요 👏🏻</SubTitleText>
-          <Button onClick={closeModal}>닫기</Button>
+          <SubTitleText>작은 목표들이 모여 큰 성과를 이뤘어요</SubTitleText>
+          <StyledImage src={BirthdayIcon} alt="축하 아이콘" />
+          <ButtonContainer>
+            {/* 버튼을 중앙에 정렬 */}
+            <Button onClick={closeModal}>닫기</Button>
+          </ButtonContainer>
         </StyledModal>
       )}
     </>
@@ -92,3 +46,72 @@ function CompletedPopup({ isCompleted }) {
 }
 
 export default CompletedPopup;
+
+const StyledModal = styled(Modal)`
+  width: 300px;
+  height: 300px;
+  background-color: white;
+  margin: auto;
+  padding: 20px;
+  border-radius: 10px;
+  border: 2px solid #007bff;
+  text-align: center;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.75);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleText = styled.p`
+  width: 100%;
+  font-size: 20px;
+  font-weight: var(--weight-bold);
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+const SubTitleText = styled.p`
+  font-size: 16px;
+  font-weight: var(--weight-semi-bold);
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  border: none;
+  border: 1px solid #216dff;
+  color: #216dff;
+  border-radius: 5px;
+  font-size: 16px;
+  margin-top: 5px;
+  cursor: pointer;
+  font-weight: var(--weight-semi-bold);
+
+  &:hover {
+    background-color: #216dff;
+    color: white;
+  }
+`;
+
+const StyledImage = styled.img`
+  width: 100px;
+  height: auto;
+  margin-top: 20px;
+`;
