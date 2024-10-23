5ff619b8c205665e59330a4358d98f6a8faaf69a
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-10-23 15:54:57 +0900

    style : 챌린지 상세페이지 배경 이미지 수정

--- src/components/challenge-popup/ChallengePopup.jsx
+++ src/components/challenge-popup/ChallengePopup.jsx
@@ -25,7 +25,7 @@ import moment from "moment";
 const challengeData = {
   challenge1: {
     name: "일회용 플라스틱 줄이기 챌린지",
-    background: "/src/assets/icons/tumbler-icon.jpg",
+    background: "/assets/icons/tumbler-icon.jpg",
     icon: "/assets/icons/challenge-1.svg",
     date: "24.11.01 - 24.11.30",
     participants: 333,
@@ -38,7 +38,7 @@ const challengeData = {
   },
   challenge2: {
     name: "제로웨이스트 챌린지",
-    background: "/src/assets/icons/recycle-icon.jpg",
+    background: "/assets/icons/recycle-icon.jpg",
     icon: "/assets/icons/challenge-2.svg",
     date: "24.11.01 - 24.11.30",
     participants: 1030,
@@ -51,7 +51,7 @@ const challengeData = {
   },
   challenge3: {
     name: "가까운 거리 걸어가기 챌린지",
-    background: "/src/assets/icons/walk-bg-icon.jpg",
+    background: "/assets/icons/walk-bg-icon.jpg",
     icon: "/assets/icons/challenge-3.svg",
     date: "24.11.01 - 24.11.30",
     participants: 730,
@@ -64,7 +64,7 @@ const challengeData = {
   },
   challenge4: {
     name: "낭비 없는 샤워 챌린지",
-    background: "/src/assets/icons/shower-icon.avif",
+    background: "/assets/icons/shower-icon.jpg",
     icon: "/assets/icons/challenge-4.svg",
     date: "24.11.01 - 24.11.30",
     participants: 407,
@@ -72,12 +72,12 @@ const challengeData = {
       "주 5회 샤워 시간을 줄여 물 낭비를 최소화하고, 물을 절약함으로써 환경 보호에 기여하는 챌린지입니다. 많은 양의 물이 샤워 중 낭비되기 쉬운데, 이를 줄이기 위해 샤워 시간을 5분 이내로 관리하며 지속 가능한 생활 습관을 형성하는 것이 목표입니다!",
     methods:
       "• 샤워를 시작하기 전 타이머를 설정해 5분 이내로 샤워 시간을 관리하세요. <br /> • 비누칠을 할 때는 물을 잠그고, 씻는 순서를 미리 계획해 시간을 줄이세요. <br />• 물 사용량을 줄일 수 있는 저수압 샤워기를 사용해 절약을 실천하세요. <br />",
-    precautions:
+    precautionㅍㄷs:
       "• 짧은 샤워 시간을 위해 지나치게 차가운 물이나 뜨거운 물을 사용하지 않도록 주의하세요. 적정 온도의 물을 사용해 피부와 건강을 보호하세요. <br /> • 짧은 시간 내에 샤워를 완료하려고 너무 무리하지 마세요. 건강과 위생을 최우선으로 유지하세요. <br /> • 수압이 너무 세지 않게 조절하여 물이 낭비되지 않도록 신경 쓰고 물 절약에 도움이 되는 샤워기와 욕실 장비를 사용해보세요.",
   },
   challenge5: {
     name: "채식 하루 도전 챌린지",
-    background: "/src/assets/icons/vegan-icon.jpg",
+    background: "/assets/icons/vegan-icon.jpg",
     icon: "/assets/icons/challenge-5.svg",
     date: "24.11.01 - 24.11.30",
     participants: 1217,
